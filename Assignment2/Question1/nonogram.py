fb4f777ac3d117773ec59a9a73e9db7ae95d6f08
WilsonZhu <winniecmz.zhu@gmail.com>
2022-11-04 16:13:27 -0400

    backup

--- Assignment2/Question1/nonogram.py
+++ Assignment2/Question1/nonogram.py
@@ -47,150 +47,6 @@ all_example_ls = {
 }
 
 
-class solveNonogram:
-    def __init__(self,
-                 row_profile=[],
-                 column_profile=[],
-                 savepath=''):
-
-        # handle the row profile
-        self.row_profile = row_profile
-        self.row_length = len(row_profile)
-        self.rows_changed = self.row_length * [0]
-        self.rows_done = self.row_length * [0]
-
-        # handle the column profile
-        self.COLS_VALUES = column_profile
-        self.num_of_columns = len(column_profile)
-        self.cols_changed = self.num_of_columns * [0]
-        self.cols_done = self.num_of_columns * [0]
-
-        self.solved = False
-        self.shape = (self.row_length, self.num_of_columns)
-        self.board = [[0 for c in range(self.num_of_columns)] for r in range(self.row_length)]
-        self.save_path = savepath
-
-        # step 1: Defining all possible solutions for every row and col
-        self.rows_possibilities = self.create_possibilities(row_profile, self.num_of_columns)
-        self.cols_possibilities = self.create_possibilities(column_profile, self.row_length)
-
-        start_time = time.time()  # mark the start time
-        while self.solved == False:
-
-            # step 2: Order indici by lowest
-            self.lowest_rows = self.select_index_not_done(self.rows_possibilities, 1)
-            self.lowest_cols = self.select_index_not_done(self.cols_possibilities, 0)
-            self.lowest = sorted(self.lowest_rows + self.lowest_cols, key=lambda element: element[1])
-
-            # step 3: Get only zeroes or only ones of the lowest possibility
-            for ind1, _, row_ind in self.lowest:
-                if not self.check_done(row_ind, ind1):
-                    if row_ind:
-                        values = self.rows_possibilities[ind1]
-                    else:
-                        values = self.cols_possibilities[ind1]
-                    same_ind = self.get_only_one_option(values)
-                    for ind2, val in same_ind:
-                        if row_ind:
-                            ri, ci = ind1, ind2
-                        else:
-                            ri, ci = ind2, ind1
-                        if self.board[ri][ci] == 0:
-                            self.board[ri][ci] = val
-                            if row_ind:
-                                self.cols_possibilities[ci] = self.remove_possibilities(self.cols_possibilities[ci], ri,
-                                                                                        val)
-                            else:
-                                self.rows_possibilities[ri] = self.remove_possibilities(self.rows_possibilities[ri], ci,
-                                                                                        val)
-                            clear_output(wait=True)
-                            # self.display_board()
-                            if self.save_path != '':
-                                self.save_board()
-                                self.n += 1
-                    self.update_done(row_ind, ind1)
-            self.check_solved()
-
-        end_time = time.time()  # mark the end time
-        print("This example takes %.4f second to be finished."
-              % (end_time - start_time))
-
-        self.display_board()
-
-    def create_possibilities(self, values, no_of_other):
-        possibilities = []
-
-        for v in values:
-            groups = len(v)
-            no_empty = no_of_other - sum(v) - groups + 1
-            ones = [[1] * x for x in v]
-            res = self._create_possibilities(no_empty, groups, ones)
-            possibilities.append(res)
-
-        return possibilities
-
-    def _create_possibilities(self, n_empty, groups, ones):
-        res_opts = []
-        for p in combinations(range(groups + n_empty), groups):
-            selected = [-1] * (groups + n_empty)
-            ones_idx = 0
-            for val in p:
-                selected[val] = ones_idx
-                ones_idx += 1
-            res_opt = [ones[val] + [-1] if val > -1 else [-1] for val in selected]
-            res_opt = [item for sublist in res_opt for item in sublist][:-1]
-            res_opts.append(res_opt)
-        return res_opts
-
-    def select_index_not_done(self, possibilities, row_ind):
-        s = [len(i) for i in possibilities]
-        if row_ind:
-            return [(i, n, row_ind) for i, n in enumerate(s) if self.rows_done[i] == 0]
-        else:
-            return [(i, n, row_ind) for i, n in enumerate(s) if self.cols_done[i] == 0]
-
-    def get_only_one_option(self, values):
-        return [(n, np.unique(i)[0]) for n, i in enumerate(np.array(values).T) if len(np.unique(i)) == 1]
-
-    def remove_possibilities(self, possibilities, i, val):
-        return [p for p in possibilities if p[i] == val]
-
-    def display_board(self):
-        plt.imshow(self.board, cmap='Greys')
-        plt.axis('off')
-        plt.show()
-
-    def save_board(self, increase_size=20):
-        name = f'0000000{str(self.n)}'[-8:]
-        increased_board = np.zeros(np.array((self.row_length, self.num_of_columns)) * increase_size)
-        for j in range(self.row_length):
-            for k in range(self.num_of_columns):
-                increased_board[j * increase_size: (j + 1) * increase_size,
-                k * increase_size: (k + 1) * increase_size] = self.board[j][k]
-        plt.imsave(os.path.join(self.save_path, f'{name}.jpeg'), increased_board, cmap='Greys', dpi=1000)
-
-    def update_done(self, row_ind, idx):
-        if row_ind:
-            vals = self.board[idx]
-        else:
-            vals = [row[idx] for row in self.board]
-        if 0 not in vals:
-            if row_ind:
-                self.rows_done[idx] = 1
-            else:
-                self.cols_done[idx] = 1
-
-    def check_done(self, row_ind, idx):
-        if row_ind:
-            return self.rows_done[idx]
-        else:
-            return self.cols_done[idx]
-
-    def check_solved(self):
-        if 0 not in self.rows_done and 0 not in self.cols_done:
-            self.solved = True
-
-
 def is_protected(test_folder_location):
     if test_folder_location in protected_folder_location_list:
         return True
@@ -206,8 +62,8 @@ def to_empty(folder_location):
         print('%s folder should not be emptied' % folder)
         return
     else:
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
+        for fileName in os.listdir(folder):
+            file_path = os.path.join(folder, fileName)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.unlink(file_path)
@@ -225,7 +81,172 @@ def to_solve(example, output_route):
     the_row = ls[0]  # extract the row list
     the_column = ls[1]  # extract the column list
 
-    solveNonogram(row_profile=the_row, column_profile=the_column, savepath=output_route)
+    SolveNonogram(row_value=the_row, column_value=the_column, savepath=output_route)
+
+
+def find_rest_options(empty_space, groups, ones):
+    rest_option_ls = []
+    for p in combinations(range(groups + empty_space), groups):
+        selected = (groups + empty_space) * [-1]
+        ones_idx = 0
+        for val in p:
+            selected[val] = ones_idx
+            ones_idx += 1
+        res_opt = [ones[val] + [-1] if val > -1 else [-1] for val in selected]
+        res_opt = [item for sublist in res_opt for item in sublist][:-1]
+        rest_option_ls.append(res_opt)
+    return rest_option_ls
+
+
+# Explore all the possible options with the given rows and column profile
+def explore_options(values, no_of_other):
+    options = []
+
+    for single_value in values:
+        groups = len(single_value)
+        no_empty = no_of_other - sum(single_value) - groups + 1
+        ones = [[1] * cell for cell in single_value]
+        raw_results = find_rest_options(no_empty, groups, ones)
+        options.append(raw_results)
+
+    return options
+
+
+def get_only_one_option(values):
+    return [(n, np.unique(i)[0]) for n, i in enumerate(np.array(values).T) if len(np.unique(i)) == 1]
+
+
+def delete_conflict_options(all_options, idx, value_ls):
+    # option_ls=[]
+    # for idx,element in enumerate (all_options):
+    #     if element[idx]==value_ls:
+    #         option_ls.append(element[idx])
+    return [option_ls for option_ls in all_options if option_ls[idx] == value_ls]
+
+
+class SolveNonogram:
+    def __init__(self,
+                 row_value=[],
+                 column_value=[],
+                 savepath=''):
+
+        # handle the row profile
+
+        self.row_profile = row_value
+        self.row_num = len(row_value)
+        self.rows_options = None  # record all the possible options to fill out a row
+        self.min_rows = None  # record the minimum possible row expansion
+        self.rows_changed = self.row_num * [0]
+        self.rows_finished = self.row_num * [0]
+
+        # handle the column profile
+        self.column_profile = column_value
+        self.column_num = len(column_value)
+        self.columns_options = None
+        self.min_columns = None
+        self.columns_changed = self.column_num * [0]
+        self.column_finished = self.column_num * [0]
+
+        self.min_option = None
+        self.isSolved = False
+        self.shape = (self.row_num, self.column_num)
+        self.board = [[0 for c in range(self.column_num)] for r in range(self.row_num)]
+        self.save_path = savepath
+
+        if self.save_path != '':
+            self.n = 0
+
+        self.solving_work()
+
+    def select_index_not_done(self, options, row_idx):
+        num_of_selections = [len(i) for i in options]
+
+        if row_idx:
+            return [(idx, n, row_idx) for idx, n in enumerate(num_of_selections) if self.rows_finished[idx] == 0]
+        else:
+            return [(i, n, row_idx) for i, n in enumerate(num_of_selections) if self.column_finished[i] == 0]
+
+    def display_board(self):
+        plt.imshow(self.board, cmap='Greys')
+        plt.axis('off')
+        plt.show()
+
+    def save_board(self, increase_size=20):
+        name = f'0000000{str(self.n)}'[-8:]
+        increased_board = np.zeros(np.array((self.row_num, self.column_num)) * increase_size)
+        for j in range(self.row_num):
+            for k in range(self.column_num):
+                increased_board[j * increase_size: (j + 1) * increase_size,
+                k * increase_size: (k + 1) * increase_size] = self.board[j][k]
+        plt.imsave(os.path.join(self.save_path, f'{name}.jpeg'), increased_board, cmap='Greys', dpi=1000)
+
+    def update_done(self, row_ind, idx):
+        if row_ind:
+            vals = self.board[idx]
+        else:
+            vals = [row[idx] for row in self.board]
+        if 0 not in vals:
+            if row_ind:
+                self.rows_finished[idx] = 1
+            else:
+                self.column_finished[idx] = 1
+
+    def check_done(self, row_ind, idx):
+        if row_ind:
+            return self.rows_finished[idx]
+        else:
+            return self.column_finished[idx]
+
+    def check_solved(self):
+        if 0 not in self.rows_finished and 0 not in self.column_finished:
+            self.isSolved = True
+
+    def solving_work(self):
+
+        self.rows_options = explore_options(self.row_profile, self.column_num)
+        self.columns_options = explore_options(self.column_profile, self.row_num)
+
+        start_time = time.time()  # mark the start time
+        while not self.isSolved:
+
+            self.min_rows = self.select_index_not_done(self.rows_options, 1)
+            self.min_columns = self.select_index_not_done(self.columns_options, 0)
+
+            self.min_option = sorted(self.min_rows + self.min_columns, key=lambda element: element[1])
+
+            for ind1, _, row_ind in self.min_option:
+                if not self.check_done(row_ind, ind1):
+                    if row_ind:
+                        values = self.rows_options[ind1]
+                    else:
+                        values = self.columns_options[ind1]
+                    same_ind = get_only_one_option(values)
+                    for ind2, val in same_ind:
+                        if row_ind:
+                            ri, ci = ind1, ind2
+                        else:
+                            ri, ci = ind2, ind1
+                        if self.board[ri][ci] == 0:
+                            self.board[ri][ci] = val
+                            if row_ind:
+                                self.columns_options[ci] = delete_conflict_options(self.columns_options[ci], ri,
+                                                                                   val)
+                            else:
+                                self.rows_options[ri] = delete_conflict_options(self.rows_options[ri], ci,
+                                                                                val)
+                            clear_output(wait=True)
+                            # self.display_board()
+                            if self.save_path != '':
+                                self.save_board()
+                                self.n += 1
+                    self.update_done(row_ind, ind1)
+            self.check_solved()
+
+        end_time = time.time()  # mark the end time
+        print("This example takes %.4f second to be finished."
+              % (end_time - start_time))
+
+        self.display_board()
 
 
 if __name__ == "__main__":
